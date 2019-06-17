06d8d33d3bd3851df52c70edc981d2e4c1f3a9c1
Florent Poinsard <florent.poinsard@outlook.fr>
2019-06-17 13:56:05 +0200

    Addition of the home template and other divers template

--- src/template/page.go
+++ src/template/page.go
@@ -1,16 +1,23 @@
 package template
 
+import (
+	"google.golang.org/appengine"
+)
+
 // Page represents a data structure used to render information
 // on a Go template
 type Page struct {
 	PageName   string
 	Production bool
+	LoggedIn   bool
 	Data       interface{}
 }
 
 // NewPage returns a new page composed of the given data
 func NewPage(pageName string, data interface{}) Page {
 	return Page{PageName: pageName,
-		Data: data,
+		Data:       data,
+		Production: !appengine.IsDevAppServer(),
+		LoggedIn:   false,
 	}
 }
