0ab4d90c48fc6b09e0e50035733c5e0beb359ab0
Florent Poinsard <florent.poinsard@outlook.fr>
2019-06-21 10:42:40 +0200

    Addition of the routes for task getters

--- src/server/task.go
+++ src/server/task.go
@@ -1,13 +1,75 @@
 package server
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/pedafy/pedafy/src/server/user"
+	"github.com/pedafy/pedafy/src/template"
+)
+
+type taskPageInfo struct {
+	Tasks  []Task
+	Status []Status
+}
 
 func (s *Server) taskHomeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello world"))
+	user, loggedIn := user.GetUser(r)
+
+	ts, err := s.taskGetAll()
+	if err != nil {
+		log.Println(err.Error())
+		ts = []Task{}
+	}
+
+	sts, err := s.taskStatusGetAll()
+	if err != nil {
+		log.Println(err.Error())
+		sts = []Status{}
+	}
+
+	data := taskPageInfo{
+		Tasks:  ts,
+		Status: sts,
+	}
+
+	p := template.NewPage("Pedafy - Tasks", loggedIn == nil, user, data)
+	if err := template.RenderTemplate(w, p, "tasks.gohtml"); err != nil {
+		log.Fatal(err.Error())
+	}
 }
 
 func (s *Server) taskHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello world"))
+	user, loggedIn := user.GetUser(r)
+
+	vars := mux.Vars(r)
+	ids := vars["id"]
+	taskID, _ := strconv.Atoi(ids)
+
+	ts, err := s.taskGetOne(taskID)
+	if err != nil {
+		log.Println(err.Error())
+		ts = []Task{}
+	}
+
+	sts, err := s.taskStatusGetAll()
+	if err != nil {
+		log.Println(err.Error())
+		sts = []Status{}
+	}
+
+	data := taskPageInfo{
+		Tasks:  ts,
+		Status: sts,
+	}
+
+	p := template.NewPage(fmt.Sprintf("Pedafy - Task #%d", taskID), loggedIn == nil, user, data)
+	if err := template.RenderTemplate(w, p, "one_task.gohtml"); err != nil {
+		log.Fatal(err.Error())
+	}
 }
 
 func (s *Server) modifyTaskHandler(w http.ResponseWriter, r *http.Request) {
@@ -18,4 +80,10 @@ func (s *Server) newTaskHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hello world"))
 }
 
+func (s *Server) modifyTaskHandlerAPI(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello world"))
+}
 
+func (s *Server) newTaskHandlerAPI(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello world"))
+}
